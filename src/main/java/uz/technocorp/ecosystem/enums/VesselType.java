768500d85e8291f55e490ea972e94c9554eb2e9a
komilrasulov1990 <komilrasulov1990@gmail.com>
2025-02-20 14:56:22 +0500

    device types

---
+++ src/main/java/uz/technocorp/ecosystem/enums/VesselType.java
@@ -0,0 +1,17 @@
+package uz.technocorp.ecosystem.enums;
+
+/**
+ * @author Rasulov Komil
+ * @version 1.0
+ * @created 20.02.2025
+ * @since v1.0
+ */
+public enum VesselType {
+
+    Steam,
+    AccumulatorCylinder,
+    MobileVessel,
+    AirCollector,
+    Receiver,
+    Separator;
+}
