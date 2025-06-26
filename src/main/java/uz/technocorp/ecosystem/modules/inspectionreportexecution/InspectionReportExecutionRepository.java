e33e72334283c41823108ed0d795cba73ffc8194
komilrasulov1990 <komilrasulov1990@gmail.com>
2025-06-26 14:37:39 +0500

    change string to uuid in dynamicSearch of appeal

--- src/main/java/uz/technocorp/ecosystem/modules/inspectionreportexecution/InspectionReportExecutionRepository.java
+++ src/main/java/uz/technocorp/ecosystem/modules/inspectionreportexecution/InspectionReportExecutionRepository.java
@@ -1,6 +1,7 @@
 package uz.technocorp.ecosystem.modules.inspectionreportexecution;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
@@ -11,4 +12,13 @@ import java.util.UUID;
  * @since v1.0
  */
 public interface InspectionReportExecutionRepository extends JpaRepository<InspectionReportExecution, UUID> {
+
+    @Query(value = """
+            select count(distinct r.id)
+            from inspection_report_execution ire
+            join inspection_report r on ire.report_id = r.id
+            and r.inspection_id = :inspectionId
+            and status <> 'ACCEPTED'
+            """, nativeQuery = true)
+    Integer getCountNotAcceptedReports(UUID inspectionId);
 }
---
+++ src/main/java/uz/technocorp/ecosystem/utils/GlobalSchedule.java
@@ -0,0 +1,16 @@
+package uz.technocorp.ecosystem.utils;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Rasulov Komil
+ * @version 1.0
+ * @created 26.06.2025
+ * @since v1.0
+ */
+@Service
+public class GlobalSchedule {
+
+    // TODO tekshiruvlarni har kuni 00:01 da EXPIRED ga tekshirish
+    // TODO tekshiruvlarni har kuni 00:01 da UNRESOLVED ga tekshirish
+}
