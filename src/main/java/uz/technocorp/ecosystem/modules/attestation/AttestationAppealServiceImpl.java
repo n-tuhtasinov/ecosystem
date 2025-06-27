da24b9187592624e247afab94a42f61d795833bc
suxrob <suxrobbek997@gmail.com>
2025-06-27 21:00:33 +0500

    changes attestation

--- src/main/java/uz/technocorp/ecosystem/modules/attestation/AttestationAppealServiceImpl.java
+++ src/main/java/uz/technocorp/ecosystem/modules/attestation/AttestationAppealServiceImpl.java
@@ -10,6 +10,8 @@ import uz.technocorp.ecosystem.modules.appeal.view.AppealViewById;
 import uz.technocorp.ecosystem.modules.attestation.dto.AttestationPendingParamsDto;
 import uz.technocorp.ecosystem.modules.attestation.enums.AttestationStatus;
 import uz.technocorp.ecosystem.modules.employee.enums.EmployeeLevel;
+import uz.technocorp.ecosystem.modules.profile.Profile;
+import uz.technocorp.ecosystem.modules.profile.ProfileService;
 import uz.technocorp.ecosystem.modules.user.User;
 
 import java.util.List;
@@ -30,11 +32,12 @@ public class AttestationAppealServiceImpl implements AttestationAppealService {
     private final AttestationSpecification specification;
     private final AttestationRepository repository;
     private final AppealService appealService;
+    private final ProfileService profileService;
 
     @Override
     public Page<AppealViewById> getAllPending(User user, AttestationPendingParamsDto dto) {
         return switch (user.getRole()) {
-            case MANAGER, HEAD -> getAllPendingForCommittee(user, dto);
+            case MANAGER, HEAD -> getAllPendingForCommittee(dto);
             case REGIONAL -> getAllPendingForRegional(user, dto);
             case INSPECTOR -> getAllPendingForInspector(user, dto);
             case LEGAL -> getAllPendingForLegal(user, dto);
@@ -42,35 +45,87 @@ public class AttestationAppealServiceImpl implements AttestationAppealService {
         };
     }
 
-    private Page<AppealViewById> getAllPendingForCommittee(User user, AttestationPendingParamsDto dto) {
+    private Page<AppealViewById> getAllPendingForCommittee(AttestationPendingParamsDto dto) {
         // Direction
         Specification<Attestation> hasDirection = specification.hasEmployeeLevel(EmployeeLevel.LEADER);
+
+        Specification<Attestation> spec = where(makeBaseSpecification(dto)).and(hasDirection);
+
+        Page<UUID> appealIds = repository.findDistinctAppealIds(spec, getPageable(dto));
+
+        List<AppealViewById> appeals = appealService.getByIds(appealIds.stream().toList());
+
+        return new PageImpl<>(appeals, getPageable(dto), appealIds.getTotalElements());
+    }
+
+    private Page<AppealViewById> getAllPendingForRegional(User user, AttestationPendingParamsDto dto) {
+        Integer regionId = profileService.getProfile(user.getProfileId()).getRegionId();
+        dto.setRegionId(regionId);
+
+        // Direction
+        Specification<Attestation> hasDirection = specification.hasEmployeeLevels(List.of(EmployeeLevel.TECHNICIAN, EmployeeLevel.EMPLOYEE));
+
+        // Inspector
+        Specification<Attestation> hasInspector = specification.hasExecutorId(dto.getInspectorId());
+
+        Specification<Attestation> spec = where(makeBaseSpecification(dto)).and(hasDirection).and(hasInspector);
+        Page<UUID> appealIds = repository.findDistinctAppealIds(spec, getPageable(dto));
+
+        List<AppealViewById> appeals = appealService.getByIds(appealIds.stream().toList());
+
+        return new PageImpl<>(appeals, getPageable(dto), appealIds.getTotalElements());
+    }
+
+    private Page<AppealViewById> getAllPendingForInspector(User user, AttestationPendingParamsDto dto) {
+        Integer regionId = profileService.getProfile(user.getProfileId()).getRegionId();
+        dto.setRegionId(regionId);
+
+        // Direction
+        Specification<Attestation> hasDirection = specification.hasEmployeeLevels(List.of(EmployeeLevel.TECHNICIAN, EmployeeLevel.EMPLOYEE));
+
+        // Inspector
+        Specification<Attestation> hasInspector = specification.hasExecutorId(user.getId());
+
+        Specification<Attestation> spec = where(makeBaseSpecification(dto)).and(hasDirection).and(hasInspector);
+        Page<UUID> appealIds = repository.findDistinctAppealIds(spec, getPageable(dto));
+
+        List<AppealViewById> appeals = appealService.getByIds(appealIds.stream().toList());
+
+        return new PageImpl<>(appeals, getPageable(dto), appealIds.getTotalElements());
+    }
+
+    private Page<AppealViewById> getAllPendingForLegal(User user, AttestationPendingParamsDto dto) {
+        Profile profile = profileService.getProfile(user.getProfileId());
+
+        // Tin
+        Specification<Attestation> hasLegalTin = specification.hasLegalTin(profile.getTin());
 
         // Status
         Specification<Attestation> hasStatus = specification.hasStatus(AttestationStatus.PENDING);
 
-        // Create pageable
-        Pageable pageable = PageRequest.of(dto.getPage(), dto.getSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
-
-        Specification<Attestation> spec = where(hasDirection).and(hasStatus);
-        Page<UUID> appealIds = repository.findDistinctAppealIds(spec, pageable);
+        Specification<Attestation> spec = where(hasLegalTin).and(hasStatus);
+        Page<UUID> appealIds = repository.findDistinctAppealIds(spec, getPageable(dto));
 
         List<AppealViewById> appeals = appealService.getByIds(appealIds.stream().toList());
 
-        return new PageImpl<>(appeals, pageable, appealIds.getTotalElements());
+        return new PageImpl<>(appeals, getPageable(dto), appealIds.getTotalElements());
     }
 
-    private Page<AppealViewById> getAllPendingForRegional(User user, AttestationPendingParamsDto dto) {
-        return null;
+    // Helper methods
+    private Specification<Attestation> makeBaseSpecification(AttestationPendingParamsDto dto) {
+        // Status
+        Specification<Attestation> hasStatus = specification.hasStatus(AttestationStatus.PENDING);
+
+        // Legal
+        Specification<Attestation> hasLegal = specification.hasLegal(dto.getSearch());
+
+        // Region
+        Specification<Attestation> hasRegion = specification.hasRegionId(dto.getRegionId());
+
+        return where(hasStatus).and(hasLegal).and(hasRegion);
     }
 
-    private Page<AppealViewById> getAllPendingForInspector(User user, AttestationPendingParamsDto dto) {
-        return null;
-    }
-
-    private Page<AppealViewById> getAllPendingForLegal(User user, AttestationPendingParamsDto dto) {
-
-
-        return null;
+    private Pageable getPageable(AttestationPendingParamsDto dto) {
+        return PageRequest.of(dto.getPage(), dto.getSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
     }
 }
