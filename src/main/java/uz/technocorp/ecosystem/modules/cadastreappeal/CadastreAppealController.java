b64f65df5b0eff93e702dc802a2dd65548fefbf0
Nurmuhammad Tuhtasinov <nurmuhammad.tuhtasinov@gmail.com>
2025-06-28 18:45:58 +0500

    add tempelate cadastre passport

--- src/main/java/uz/technocorp/ecosystem/modules/cadastreappeal/CadastreAppealController.java
+++ src/main/java/uz/technocorp/ecosystem/modules/cadastreappeal/CadastreAppealController.java
@@ -1,12 +1,18 @@
 package uz.technocorp.ecosystem.modules.cadastreappeal;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uz.technocorp.ecosystem.modules.appeal.AppealService;
+import uz.technocorp.ecosystem.modules.appeal.pdfservice.AppealPdfService;
+import uz.technocorp.ecosystem.modules.cadastreappeal.dto.CadastrePassportDto;
 import uz.technocorp.ecosystem.modules.user.User;
 import uz.technocorp.ecosystem.security.CurrentUser;
+import uz.technocorp.ecosystem.shared.ApiResponse;
 
 /**
  * @author Nurmuhammad Tuhtasinov
@@ -16,7 +22,15 @@ import uz.technocorp.ecosystem.security.CurrentUser;
  */
 @RestController
 @RequestMapping("/api/v1/appeals/cadastre")
+@RequiredArgsConstructor
 public class CadastreAppealController {
-//    @PostMapping
-//    public ResponseEntity<?> create(@CurrentUser User user, @Valid )
+
+    private final AppealService appealService;
+    private final AppealPdfService appealPdfService;
+
+    @PostMapping("/passport/generate-pdf")
+    public ResponseEntity<?> createPassport(@CurrentUser User user, @Valid @RequestBody CadastrePassportDto cadastrePassportDto) {
+        String path = appealPdfService.preparePdfWithParam(cadastrePassportDto, user);
+        return ResponseEntity.ok(new ApiResponse("PDF fayl yaratildi", path));
+    }
 }
